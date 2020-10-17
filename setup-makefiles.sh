ee7720605056f303dc87ca47c53bfb614f4dbcef
Martin <recordyletsplays@gmail.com>
2020-10-17 12:19:38 +0200

    sdm660-common: EvoX-ify

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,9 +26,9 @@ INITIAL_COPYRIGHT_YEAR=2018
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-AOSIP_ROOT="$MY_DIR"/../../..
+EVO_ROOT="$MY_DIR"/../../..
 
-HELPER="$AOSIP_ROOT"/vendor/aosip/build/tools/extract_utils.sh
+HELPER="$EVO_ROOT"/vendor/evolution/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -36,7 +36,7 @@ fi
 . "$HELPER"
 
 # Initialize the common helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AOSIP_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$EVO_ROOT" true
 
 # Copyright headers and guards
 write_headers "jasmine_sprout jason lavender twolip wayne whyred"
@@ -55,7 +55,7 @@ if [ -s "$MY_DIR"/../$DEVICE_SPECIFIED_COMMON/proprietary-files.txt ]; then
 
     # Reinitialize the helper for device specified common
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE_SPECIFIED_COMMON" "$VENDOR" "$AOSIP_ROOT" true
+    setup_vendor "$DEVICE_SPECIFIED_COMMON" "$VENDOR" "$EVO_ROOT" true
 
     # Copyright headers and guards
     write_headers "$DEVICE_SPECIFIED_COMMON_DEVICE"
@@ -72,7 +72,7 @@ fi
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$AOSIP_ROOT" false
+    setup_vendor "$DEVICE" "$VENDOR" "$EVO_ROOT" false
 
     # Copyright headers and guards
     write_headers
